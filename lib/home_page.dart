80a17192edd9227b168fd3a4ff0fa9779fbfeb35
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-10 00:13:21 +0500

    📝 Added Comments 🛡

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,10 +2,10 @@ import 'package:cubit_example/home_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-// Cubits are one layer above from Streams and StreamControllers
-// When we create a cubit we also make sure we dispose it
-// So we need to use StatefulWidget so that we could initialize our cubit
-// and also could dispose it
+// * Cubits are one layer above from Streams and StreamControllers
+// * When we create a cubit we also make sure we dispose it
+// * So we need to use StatefulWidget so that we could initialize our cubit
+//   and also could dispose it
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -31,8 +31,8 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  // As cubit's internal state changes we need to update our homepage's state =>
-  // Use StreamBuilder managing value of nullable String just like our cubit!
+  // * As cubit's internal state changes we need to update our homepage's state =>
+  //   Use StreamBuilder managing value of nullable String just like our cubit!
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,14 +49,14 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.black,
       ),
       body: StreamBuilder<String?>(
-        // We see the cubit is actually exposing a stream of its state,
-        // which is that stream of stream
+        // * We see the cubit is actually exposing a stream of its state,
+        //   which is that stream of stream
         stream: cubit.stream,
         builder: (context, snapshot) {
-          // Create a button here, which will call the pickRandomName
-          // on our cubit and it will ask the cubit to produce a new value upon
-          // which the stream of the cubit will produce a new value and it will
-          // come to our builder again
+          // * Create a button here, which will call the pickRandomName
+          //   on our cubit and it will ask the cubit to produce a new value upon
+          //   which the stream of the cubit will produce a new value and it will
+          //   come to our builder again
           final button = Center(
             child: TextButton(
               onPressed: cubit.pickRandomName,
@@ -76,8 +76,8 @@ class _HomePageState extends State<HomePage> {
             ),
           );
 
-          // So now we have the button we should go ahead and handle various
-          // states of the snapshot
+          // * So now we have the button we should go ahead and handle various
+          //   states of the snapshot
           switch (snapshot.connectionState) {
             case ConnectionState.none:
               return Center(
@@ -118,10 +118,10 @@ class _HomePageState extends State<HomePage> {
                 ),
               );
             case ConnectionState.active:
-              // In case our cubit is active and it's like actively producing
-              // values or is at least ready to produce new values => we're
-              // gonna create column in here that displays both button and the
-              // text that is produced by the cubit
+              // * In case our cubit is active and it's like actively producing
+              //   values or is at least ready to produce new values => we're
+              //   gonna create column in here that displays both button and the
+              //   text that is produced by the cubit
               return Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -151,10 +151,10 @@ class _HomePageState extends State<HomePage> {
                 ),
               );
             case ConnectionState.done:
-              // We should never get here because the stream of our cubit
-              // shouldn't complete or shouldn't be done until our homepage's
-              // statefulWidget has actually been disposed!
-              // So we're just putting a sizedBox in here
+              // * We should never get here because the stream of our cubit
+              //   shouldn't complete or shouldn't be done until our homepage's
+              //   statefulWidget has actually been disposed!
+              //   So we're just putting a sizedBox in here
               return const SizedBox();
           }
         },
