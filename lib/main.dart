d4039ef02b82b19929081eec982484f678650338
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-09 23:53:40 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:cubit_example/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        title: 'Cubit Example',
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+  );
+}
